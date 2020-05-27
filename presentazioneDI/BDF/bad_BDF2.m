3f761c5675c88a96a80dd29aa51eb9c8f945d755
5N44P <valerionappi995@gmail.com>
2020-05-27 10:48:06 +0200

    aggiunto submodulo overleaf, aggiunti esempi di cattivo priming BDF

---
+++ presentazioneDI/BDF/bad_BDF2.m
@@ -0,0 +1,19 @@
+function y = bad_BDF2(f,t,y0)
+%BDF1 Summary of this function goes here
+%   Detailed explanation goes here
+
+h = diff(t);
+y = zeros(numel(y0),numel(t));
+
+y(:,1:2) = BDF1(f,t(1:2),y0);
+
+for ii = 2:numel(t)-1
+    BDF2 = @(x)  (4/3).* y(:,ii) - (1/3).* y(:,ii-1) ...
+                    + (2/3) .* h(ii).*f(t(ii+1),x) - x;
+                
+    y(:,ii+1) = fsolve(BDF2, y(:,ii),optimset('Display','off'));
+end
+
+
+end
+
---
+++ presentazioneDI/BDF/bad_BDF3.m
@@ -0,0 +1,17 @@
+function y = bad_BDF3(f,t,y0)
+%BDF1 Summary of this function goes here
+%   Detailed explanation goes here
+
+h = diff(t);
+y = zeros(numel(y0),numel(t));
+
+y(:,1:3) = BDF2(f,t(1:3),y0);
+
+for ii = 3:numel(t)-1
+    BDF3 = @(x) (18/11).*y(:, ii) - (9/11).*y(:,ii-1) + ...
+    (2/11).*y(:,ii-2)  + (6/11 ).* h(ii).*f(t(ii+1),x) - x;
+    y(:,ii+1) = fsolve(BDF3, y(:,ii),optimset('Display','off'));
+end
+
+end
+
